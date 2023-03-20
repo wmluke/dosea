91a34f93efb30e81335728357048f89f3d827aae
Luke Bunselmeyer <luke@bunselmeyer.net>
2023-03-19 23:28:42 -0400

    Updated package-lock.json and removed servermoduleformat from remix

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
     cacheDirectory: "./node_modules/.cache/remix",
     ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
     serverDependenciesToBundle: [/^echarts.*/, /^zrender.*/],
-    servermoduleformat: "esm",
+    // servermoduleformat: "esm",
     future: {
         unstable_tailwind: false,
         v2_routeConvention: true,
