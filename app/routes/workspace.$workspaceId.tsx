c6870805a1b99abc5c11586dcf8c170ba42229e2
Luke Bunselmeyer <luke@bunselmeyer.net>
2023-03-21 09:57:06 -0400

    [#3] Added secondary drawer on left side

--- app/routes/workspace.$workspaceId.tsx
+++ app/routes/workspace.$workspaceId.tsx
@@ -51,36 +51,53 @@ export default function WorkspacePage() {
     const queryLoaderData = queryPageLoaderData ?? chartsPageLoaderData;
 
     return (
-        <div className="drawer-mobile drawer">
-            <input id="main-drawer" type="checkbox" className="drawer-toggle" />
-            <div
-                className="drawer-content flex flex-col"
-                style={{ scrollBehavior: "smooth", scrollPaddingTop: "5rem" }}
+        <div className="drawer-mobile drawer drawer-primary">
+            <input id="primary-drawer" type="checkbox" className="drawer-toggle drawer-primary-toggle" />
+            <div className="drawer-content drawer-primary-content flex flex-col"
+                 style={{ scrollBehavior: "smooth", scrollPaddingTop: "5rem" }}
             >
-                <div className="navbar bg-base-100 lg:hidden">
-                    <div className="flex-none lg:hidden">
-                        <label htmlFor="main-drawer" className="btn-ghost btn-square btn">
-                            <Bars3Icon className="inline-block h-6 w-6 stroke-current" />
-                        </label>
+                <div className="drawer-mobile drawer drawer-end drawer-secondary">
+                    <input id="secondary-drawer" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content drawer-secondary-content flex flex-col"
+                         style={{ scrollBehavior: "smooth", scrollPaddingTop: "5rem" }}
+                    >
+                        <div className="navbar bg-base-100 lg:hidden">
+                            <div className="flex-none lg:hidden">
+                                <label htmlFor="primary-drawer" className="btn-ghost btn-square btn">
+                                    <Bars3Icon className="inline-block h-6 w-6 stroke-current" />
+                                </label>
+                            </div>
+                            <div className="mx-2 flex-1 px-2 text-3xl lg:hidden">{workspace?.name}</div>
+                            <div className="flex-none">
+                                <div className="flex-none lg:hidden">
+                                    <label htmlFor="secondary-drawer" className="btn-ghost btn-square btn">
+                                        <Bars3Icon className="inline-block h-6 w-6 stroke-current" />
+                                    </label>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div className="mx-2 my-2 px-2 bg-base-100 bg-opacity-90">
+                            <Outlet></Outlet>
+                        </div>
                     </div>
-                    <div className="mx-2 flex-1 px-2 text-3xl lg:hidden">{workspace?.name}</div>
-                    <div className="flex-none"></div>
-                </div>
-                <div className="mx-2 my-2 flex justify-between gap-10 bg-base-100 bg-opacity-90 px-2">
-                    <div className="grow basis-4/5">
-                        <Outlet></Outlet>
-                    </div>
-                    <div className="prose grow-0 basis-1/5 hidden sm:block">
-                        <RightPane
-                            queryResult={queryLoaderData?.queryResult?.result}
-                            queryError={queryLoaderData?.queryResult?.error}
-                            tables={tables}
-                        />
+                    <div className="drawer-side drawer-secondary-side"
+                         style={{ scrollBehavior: "smooth", scrollPaddingTop: "5rem" }}>
+                        <label htmlFor="secondary-drawer" className="drawer-overlay drawer-overlay-secondary"></label>
+                        <div className="prose bg-base-200 px-4">
+                            <RightPane
+                                queryResult={queryLoaderData?.queryResult?.result}
+                                queryError={queryLoaderData?.queryResult?.error}
+                                tables={tables}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="drawer-side" style={{ scrollBehavior: "smooth", scrollPaddingTop: "5rem" }}>
-                <label htmlFor="main-drawer" className="drawer-overlay"></label>
+            <div className="drawer-side drawer-primary-side"
+                 style={{ scrollBehavior: "smooth", scrollPaddingTop: "5rem" }}>
+                <label htmlFor="primary-drawer" className="drawer-overlay"></label>
                 <LeftNav workspace={workspace} dataset={dataset} />
             </div>
         </div>
