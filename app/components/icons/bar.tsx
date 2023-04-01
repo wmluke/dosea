43151163a8b553d7af78dfa0d332762cba37806b
Luke Bunselmeyer <luke@bunselmeyer.net>
2023-04-01 00:42:28 -0400

    [#8] Fixed lint issue in chart icon components

--- app/components/icons/bar.tsx
+++ app/components/icons/bar.tsx
@@ -1,5 +1,5 @@
+import type { SVGProps } from "react";
 import * as React from "react";
-import { SVGProps } from "react";
 
 const SvgBar = (props: SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 175 138" {...props}>
--- app/components/icons/custom.tsx
+++ app/components/icons/custom.tsx
@@ -1,5 +1,5 @@
+import type { SVGProps } from "react";
 import * as React from "react";
-import { SVGProps } from "react";
 
 const SvgCustom = (props: SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 159 142" {...props}>
